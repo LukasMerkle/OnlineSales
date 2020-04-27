711b8106047c6be975995311027b050c6ec6aa36
lmerkle <lmerkle@andrew.cmu.edu>
2020-04-27 18:22:45 -0400

    converted toclass: :

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,20 @@
 import numpy as np
 import pandas as pd
 
+
+class Analyzer():
+    def __init__(self,filename):
+        self.data=pd.read_csv(filename, sep=',')
+        self.products = self.data['product'].unique()
+        self.outcome = self.data['title'].unique()
+        self.site_version = self.data['site_version'].unique()
+
+    def analyze_categories(self):
+        for category in self.products:
+            c = data[data['product'].str.match(category)]
+            # for criteria in ['']
+
 if __name__ == "__main__":
-    df=pd.read_csv('../data/product_sample.csv', sep=',',header=None)
-    print(df)
+    analyzer = Analyzer('../data/product_sample.csv')
+    
+        
