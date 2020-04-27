d036d32560ef7bb96ee38b4cc9e7e24a69061c41
lmerkle <lmerkle@andrew.cmu.edu>
2020-04-27 18:33:57 -0400

    wrote category analyzes

--- scripts/main.py
+++ scripts/main.py
@@ -8,13 +8,16 @@ class Analyzer():
         self.products = self.data['product'].unique()
         self.outcome = self.data['title'].unique()
         self.site_version = self.data['site_version'].unique()
+        self.columns = [['order_id', 'user_id', 'page_id', 'product', 'site_version', 'time','title', 'target']]
 
     def analyze_categories(self):
         for category in self.products:
-            c = data[data['product'].str.match(category)]
-            # for criteria in ['']
-
+            c = self.data[self.data['product'].str.match(category)]
+            for outcome in self.outcome:
+                outcome_result = c[c['title'].str.match(outcome)]
+                print("Probabilty of",outcome,"given the category",category,"is",outcome_result.shape[0]/c.shape[0])
 if __name__ == "__main__":
     analyzer = Analyzer('../data/product_sample.csv')
+    analyzer.analyze_categories()
     
         
